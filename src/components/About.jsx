0c5f0246050ed853f0647da52eef4462365e3b1f
KJcode5 <kayasmith194@gmail.com>
2022-06-03 23:37:19 +0100

    faker Lorem paragraph added to About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { faker } from '@faker-js/faker';
 
 const About = () => {
     return (
       <div className="container container-about-us">
         <h2>Welcome to PurpleBeard Weather App</h2>
         <img src="https://purplebeard.co.uk/wp-content/uploads/2019/07/Finalwhite-300x108.png" width={300}></img>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et sem et ligula efficitur viverra ut vel sapien. Maecenas a ornare metus. Vivamus sapien quam, tempor congue pretium eget, euismod at est. Ut accumsan imperdiet nisi ac faucibus. Morbi tortor sapien, ultrices in efficitur ut, tempor a erat. Nunc blandit tempor egestas. Vestibulum maximus consequat metus eu vulputate.</p>
+        <div className='faker-lorem'>{faker.lorem.lines(15)}</div>
         <a class="button" href="https://purplebeard.co.uk/" target={"_blank"}>Hire a Developer</a>
         <a class="button" href="/">Back</a>
       </div>
