92321014cd8d89268b7e6f75289131928e7d840a
Paul C <civas_1992@yahoo.com>
2022-06-03 23:36:06 +0100

    CSS library Bootstrap
    https://getbootstrap.com/docs/5.2/components/card/

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 
 const About = () => {
     return (
+      // we used bootstrap card component
+      // https://getbootstrap.com/docs/5.2/components/card/
       <div className="container container-about-us">
-        <h2>Welcome to PurpleBeard Weather App</h2>
-        <img src="https://purplebeard.co.uk/wp-content/uploads/2019/07/Finalwhite-300x108.png" width={300}></img>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et sem et ligula efficitur viverra ut vel sapien. Maecenas a ornare metus. Vivamus sapien quam, tempor congue pretium eget, euismod at est. Ut accumsan imperdiet nisi ac faucibus. Morbi tortor sapien, ultrices in efficitur ut, tempor a erat. Nunc blandit tempor egestas. Vestibulum maximus consequat metus eu vulputate.</p>
-        <a class="button" href="https://purplebeard.co.uk/" target={"_blank"}>Hire a Developer</a>
-        <a class="button" href="/">Back</a>
+      <div class="card">
+      <img class="card-img-top" alt="purple-beard-logo" src="https://purplebeard.co.uk/wp-content/uploads/2019/07/Finalwhite-300x108.png" width={300}></img>
+        <div class="card-body">
+          <h2 class="card-title">Welcome to PurpleBeard Weather App</h2>
+          <p class="card-text">Lorem ipsum dolor sit amegula efficitur viverra ut vel sapien. Maecenas a ornare metus. Vivamus sapien quam, tempor congue pretium eget, euismod at est. Ut accumsan imperdiet nisi ac faucibus. Morbi tortor sapien, ultrices in efficitur ut, tempor a erat. Nunc blandit tempor egestas. Vestibulum maximus consequat metus eu vulputate.</p>
+          <a href="https://purplebeard.co.uk/" target={"_blank"} class="btn button btn-lg">Hire a Developer</a>
+          <a class="button" href="/">Back</a>
+        </div>
       </div>
+        
+      </div>
+      
     )
 }
 
